18176a138c439accd02169d04fbca0660d4410f1
Dhixon Nuñez <37592783+dhix@users.noreply.github.com>
2021-10-06 22:51:12 -0600

    Delete api_empresa_backend directory

--- api_empresa_backend/Models/Puestos.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-namespace web_api_empresa.Models{
-    public class Puestos{
-        [Key]
-        public int id_puesto { get; set; }
-        public string puesto { get; set; }
-
-    }
-}
